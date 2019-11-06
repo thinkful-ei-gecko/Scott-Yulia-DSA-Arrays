8ceca814279919fcd22d520bbf59e9e4ca68e3da
Scott Schroeder <snschroeder89@gmail.com>
2019-11-06 15:26:11 -0600

    finished!

--- nativeArrayDrills.js
+++ nativeArrayDrills.js
@@ -107,3 +107,17 @@ twoD([[1,0,1,1,0],
   [1,1,1,1,1],
   [1,0,1,1,1],
   [1,1,1,1,1]]);
+
+  function isValidRotation(str1, str2) {
+    let isValid = false;
+    for (let i = 0; i < str2.length; i < i++) {
+      let tempStr = str2.slice(i) + str2.slice(0, i)
+      console.log(tempStr);
+      if (str1 === tempStr) {
+        isValid = true;
+      }
+    }
+    return isValid
+  }
+
+  console.log(isValidRotation('amazon', 'azonma'))
