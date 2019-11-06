f304f2155ec24c899fe519e9cf1a2b72eddc9ac6
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-06 09:52:28 -0800

    complete filter function

--- nativeArrayDrills.js
+++ nativeArrayDrills.js
@@ -4,4 +4,25 @@ function urlifyString(str) {
   return strArr.join('%20'); // O(n)
 };
 
-console.log(urlifyString('www.thinkful.com /tauh ida parv een'));
+// console.log(urlifyString('www.thinkful.com /tauh ida parv een'));
+
+function filter(arr) {
+  // for (let i = 0; i< arr.length; i++) { //O(n)
+  //   if (arr[i] < 5) { //O(1)
+  //     arr.splice(i, 1); //O(n)
+  //   }
+
+  // }
+  // return arr;
+
+  let newArr = []; //0(1)
+  for (let i = 0; i< arr.length; i++) { //O(n)
+    if (arr[i] >= 5) {
+      newArr.push(arr[i]); //O(1)
+    }
+  }
+  return newArr;
+} 
+
+//console.log(filter([1, 6, 2, 8, 9]));
+
