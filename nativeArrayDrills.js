55c24c243dc0567ffc5774713ccf6fc6a9b6ecc4
Scott Schroeder <snschroeder89@gmail.com>
2019-11-06 13:26:36 -0600

    updated url

--- nativeArrayDrills.js
+++ nativeArrayDrills.js
@@ -1,7 +1,5 @@
 function urlifyString(str) {
   return str.split(" ").join('%20'); // O(n)
-  // strArr = strArr.filter(val => val !== ''); // O(n)
-  // return strArr.join('%20'); // O(n)
 };
 
 console.log(urlifyString('www.thinkful.com /tauh ida parv een'));
